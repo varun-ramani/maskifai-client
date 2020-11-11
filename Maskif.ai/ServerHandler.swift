420f2a14af931643a90985f5475e72017d197c30
Simon Chervenak <sarras305@gmail.com>
2020-11-11 11:47:18 -0500

    move from starscream => socket-io-swift

--- Maskif.ai/ServerHandler.swift
+++ Maskif.ai/ServerHandler.swift
@@ -6,29 +6,38 @@
 //
 
 import Foundation
-import Starscream
+import SocketIO
 import AVFoundation
+import UIKit
 
-let NGROK_URL = "http://7d18ed560826.ngrok.io/"
+let NGROK_URL = "https://7d18ed560826.ngrok.io/"
 
 // code translated from https://stackoverflow.com/a/6197348/6342812
-func imageToBuffer(_ source: CMSampleBuffer) -> Data {
+func imageBufferToData(_ source: CMSampleBuffer) -> Data {
     let imageBuffer = CMSampleBufferGetImageBuffer(source)!
+    let ciimage = CIImage(cvPixelBuffer: imageBuffer)
+    let context: CIContext = CIContext(options: nil)
+    let cgImage = context.createCGImage(ciimage, from: ciimage.extent)!
+    let image = UIImage(cgImage: cgImage)
+    return image.pngData()!
 
-    CVPixelBufferLockBaseAddress(imageBuffer, [])
-  
-    let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
-    let width = CVPixelBufferGetWidth(imageBuffer)
-    let height = CVPixelBufferGetHeight(imageBuffer)
-    let src_buff = CVPixelBufferGetBaseAddress(imageBuffer)
-  
-    CVPixelBufferUnlockBaseAddress(imageBuffer as CVPixelBuffer, [])
-
-    return Data(bytes: src_buff!, count: bytesPerRow * height)
+//    CVPixelBufferLockBaseAddress(imageBuffer, [])
+//
+//    let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+//    let width = CVPixelBufferGetWidth(imageBuffer)
+//    let height = CVPixelBufferGetHeight(imageBuffer)
+//    let src_buff = CVPixelBufferGetBaseAddress(imageBuffer)
+//
+//    let data = Data(bytes: src_buff!, count: bytesPerRow * height)
+//
+//    CVPixelBufferUnlockBaseAddress(imageBuffer as CVPixelBuffer, [])
+//
+//    return data
 }
 
 class ServerHandler {
-  var socket: WebSocket!
+  let manager = SocketManager(socketURL: URL(string: NGROK_URL)!, config: [.log(true), .compress])
+  var socket: SocketIOClient!
   
   static let shared: ServerHandler = ServerHandler()
   static var connected = false
@@ -39,10 +48,16 @@ class ServerHandler {
   
   func connect() {
     if !ServerHandler.connected {
-      var request = URLRequest(url: URL(string: NGROK_URL)!)
-      request.timeoutInterval = 5
-      socket = WebSocket(request: request)
-      socket.delegate = self
+      socket = manager.defaultSocket
+
+      socket.on(clientEvent: .connect) {data, ack in
+          print("socket connected")
+      }
+      
+      socket.onAny { (event) in
+        print("The event is: \(event.event)")
+      }
+      
       socket.connect()
       
       ServerHandler.connected = true
@@ -58,54 +73,6 @@ class ServerHandler {
   }
   
   func sendCameraFrame(_ buffer: CMSampleBuffer) {
-    socket.write(data: imageToBuffer(buffer))
-  }
-}
-
-extension ServerHandler: WebSocketDelegate {
-  func didReceive(event: WebSocketEvent, client: WebSocket) {
-    switch event {
-    case .connected(let headers):
-      ServerHandler.connected = true
-      print("websocket is connected: \(headers)")
-    case .disconnected(let reason, let code):
-      ServerHandler.connected = false
-      print("websocket is disconnected: \(reason) with code: \(code)")
-    case .text(let string):
-      print("Received text: \(string)")
-    case .binary(let data):
-      print("Received data: \(data.count)")
-    case .ping(_):
-      break
-    case .pong(_):
-      break
-    case .viabilityChanged(_):
-      break
-    case .reconnectSuggested(_):
-      break
-    case .cancelled:
-      ServerHandler.connected = false
-    case .error(let error):
-      ServerHandler.connected = false
-      print(error)
-      connect()
-//      handleError(error)
-    }
-  }
-  
-  func websocketDidConnect(socket: WebSocketClient) {
-    print("connected")
-  }
-  
-  func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
-    print("disconnected")
-  }
-  
-  func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
-    print("got message", text)
-  }
-  
-  func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
-    print("got data")
+    socket.emit("image", imageBufferToData(buffer))
   }
 }
