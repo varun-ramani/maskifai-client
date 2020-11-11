f5b0dc0d0acaec6d3fceea8395fb50a8fb70307d
Simon Chervenak <sarras305@gmail.com>
2020-11-11 11:20:19 -0500

    last try with starscream

--- Maskif.ai/ServerHandler.swift
+++ Maskif.ai/ServerHandler.swift
@@ -9,7 +9,7 @@ import Foundation
 import Starscream
 import AVFoundation
 
-let NGROK_URL = "https://7d18ed560826.ngrok.io"
+let NGROK_URL = "http://7d18ed560826.ngrok.io/"
 
 // code translated from https://stackoverflow.com/a/6197348/6342812
 func imageToBuffer(_ source: CMSampleBuffer) -> Data {
@@ -27,7 +27,7 @@ func imageToBuffer(_ source: CMSampleBuffer) -> Data {
     return Data(bytes: src_buff!, count: bytesPerRow * height)
 }
 
-class ServerHandler: WebSocketDelegate {
+class ServerHandler {
   var socket: WebSocket!
   
   static let shared: ServerHandler = ServerHandler()
@@ -57,13 +57,55 @@ class ServerHandler: WebSocketDelegate {
     }
   }
   
-  // todo: socket.disconnect
-  
-  func didReceive(event: WebSocketEvent, client: WebSocket) {
-    print("hm")
-  }
-  
   func sendCameraFrame(_ buffer: CMSampleBuffer) {
     socket.write(data: imageToBuffer(buffer))
+  }
+}
+
+extension ServerHandler: WebSocketDelegate {
+  func didReceive(event: WebSocketEvent, client: WebSocket) {
+    switch event {
+    case .connected(let headers):
+      ServerHandler.connected = true
+      print("websocket is connected: \(headers)")
+    case .disconnected(let reason, let code):
+      ServerHandler.connected = false
+      print("websocket is disconnected: \(reason) with code: \(code)")
+    case .text(let string):
+      print("Received text: \(string)")
+    case .binary(let data):
+      print("Received data: \(data.count)")
+    case .ping(_):
+      break
+    case .pong(_):
+      break
+    case .viabilityChanged(_):
+      break
+    case .reconnectSuggested(_):
+      break
+    case .cancelled:
+      ServerHandler.connected = false
+    case .error(let error):
+      ServerHandler.connected = false
+      print(error)
+      connect()
+//      handleError(error)
+    }
+  }
+  
+  func websocketDidConnect(socket: WebSocketClient) {
+    print("connected")
+  }
+  
+  func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+    print("disconnected")
+  }
+  
+  func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+    print("got message", text)
+  }
+  
+  func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+    print("got data")
   }
 }
